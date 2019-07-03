0fa66f690c055a9026690fcc6d1943da9d18a1bd
Par <chairat.onyaem@exxonmobil.com>
2019-07-03 15:26:11 +0800

    Add type to CheckConfig to distinguish check type (status,json)

--- src/main/java/dev/chairat/healthcheck/config/CheckConfig.java
+++ src/main/java/dev/chairat/healthcheck/config/CheckConfig.java
@@ -1,11 +1,15 @@
 package dev.chairat.healthcheck.config;
 
+import dev.chairat.healthcheck.model.ErrorGeneral;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.NestedConfigurationProperty;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @ConfigurationProperties
@@ -15,11 +19,17 @@ import org.springframework.boot.context.properties.NestedConfigurationProperty;
 public class CheckConfig {
 
     @NestedConfigurationProperty
+    private String type;
+
+    @NestedConfigurationProperty
     private String path;
 
     @NestedConfigurationProperty
     private String value;
 
     private Boolean result;
+
+    @Builder.Default
+    private List<ErrorGeneral> errors = new ArrayList<>();
 
 }
